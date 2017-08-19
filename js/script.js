1912e56665b99f11826dd2f37465b45769e6ec7e
Mohammed Fahad Kaleem <mohammedfahadkaleem@gmail.com>
2017-08-18 18:05:57 -0700

    Added Functionality

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/**
+ * Created by kaleemmf on 8/18/17.
+ */
+document.getElementById('clock').innerHTML = "LOL";
+
+function startClock(){
+    var today = new Date();
+    var hour = today.getHours();
+    var minute = today.getMinutes();
+    var seconds = today.getHours();
+}
